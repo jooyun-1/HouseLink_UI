bd561bb170f42f926ed41d914f6cd05948a49459
주윤 <joo6881@naver.com>
2023-11-16 17:45:09 +0900

    UI 초기 구성

---
+++ s1014_vue_home_kwonsunjun_jooyun/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// import { RouterView } from 'vue-router'
+// import Header from './components/Header.vue';
+// import Search from './components/Search.vue';
+
+import { RouterView } from 'vue-router'
+// import KakaoMap from './components/KakaoMap.vue'
+// import Header from './components/Header.vue'
+// import Search from './components/Search.vue'
+</script>
+
+<template>
+  <div id="app">
+    <!-- <Home> </Home> -->
+
+    <RouterView />
+  </div>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
---
+++ s1014_vue_home_kwonsunjun_jooyun/src/components/ReviewDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="form-control">
+    <label class="label">
+      <span class="label-text">Your bio</span>
+      <span class="label-text-alt">Alt label</span>
+    </label>
+    <textarea class="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+    <label class="label">
+      <span class="label-text-alt">Your bio</span>
+      <span class="label-text-alt">Alt label</span>
+    </label>
+  </div>
+</template>
+<script setup></script>
+<style lang=""></style>
