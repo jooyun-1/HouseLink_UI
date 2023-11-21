9eb8d3f9f56451c8c363e475f198c9a226e47348
권순준 <hgfdsa4320@naver.com>
2023-11-21 17:33:11 +0900

    역할에 따라 아파트 관리 기능 구현

--- s1014_vue_home_kwonsunjun_jooyun/src/components/Header.vue
+++ s1014_vue_home_kwonsunjun_jooyun/src/components/Header.vue
@@ -59,7 +59,13 @@
               </ul>
             </details>
           </li>
+          <button v-if='isAdmin' id="createAptBtn" onclick="postAptModal.showModal()" class="btn btn-neutral btn-sm">아파트 등록</button>
+          <button v-if='isAdmin' id="updateAptBtn" onclick="signUpModal.showModal()" class="btn btn-neutral btn-sm">아파트 수정</button>
+          <button v-if='isAdmin' id="deleteAptBtn" onclick="signUpModal.showModal()" class="btn btn-neutral btn-sm">아파트 삭제</button>
         </ul>
+
+        
+              
         <div class="">
           <button
             v-if="!loggedIn"
@@ -182,6 +188,121 @@
             </div>
           </div>
         </dialog>
+
+        <dialog id="postAptModal" class="modal">
+          <div class="modal-box flex flex-col justify-center w-[25rem]">
+            <form method="dialog">
+              <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            </form>
+            <label class="label">
+              <span class="label-text">아파트 등록번호</span>
+            </label>
+            <input
+              type="text"
+              id="aptNo"
+              placeholder="AptID"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트이름</span>
+            </label>
+            <input
+              type="text"
+              id="aptName"
+              placeholder="AptName"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">시/군/구 코드</span>
+            </label>
+            <input
+              type="text"
+              id="sigunguCode"
+              placeholder="Code"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">층</span>
+            </label>
+            <input
+              type="text"
+              id="floor"
+              placeholder="Floor"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">면적</span>
+            </label>
+            <input
+              type="text"
+              id="area"
+              placeholder="Area"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트 거래 연도</span>
+            </label>
+            <input
+              type="text"
+              id="dealYear"
+              placeholder="DealYear"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트 거래 월</span>
+            </label>
+            <input
+              type="text"
+              id="dealMonth"
+              placeholder="DealMonth"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트 거래 일</span>
+            </label>
+            <input
+              type="text"
+              id="dealDay"
+              placeholder="DealDay"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트 거래 가격</span>
+            </label>
+            <input
+              type="text"
+              id="dealAmount"
+              placeholder="DealAmount"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <label class="label">
+              <span class="label-text">아파트 코드</span>
+            </label>
+            <input
+              type="text"
+              id="aptCode"
+              placeholder="AptCode"
+              maxlength="20"
+              class="input input-bordered w-full max-w-md"
+            />
+            <div class="flex gap-[10px] p-4 pl-0 pb-0">
+              <button class="btn btn-primary w-1/2" style="margin: auto" @click="postApt()">
+                아파트 등록
+              </button>
+              <!-- <button class="btn btn-primary w-1/2" onclick="login()">로그인</button> -->
+            </div>
+          </div>
+        </dialog>
+
       </nav>
     </header>
   </div>
@@ -234,13 +355,56 @@ const signUp = () => {
       .then((res) => {
         if (res.status === 200) {
           loggedIn.value = true
-          console.log(res)
         }
       })
   } catch (error) {
     console.error(error)
   }
   signUpModal.close()
+}
+
+const postApt = () => {
+  const postAptModal = document.querySelector('#postAptModal')
+  const no = document.querySelector('#aptNo').value
+  const houseName = document.querySelector('#aptName').value
+  const sigunguCode = document.querySelector('#sigunguCode').value
+  const floor = document.querySelector('#floor').value
+  const area = document.querySelector('#area').value
+  const dealYear = document.querySelector('#dealYear').value
+  const dealMonth = document.querySelector('#dealMonth').value
+  const dealDay = document.querySelector('#dealDay').value
+  const dealAmount = document.querySelector('#dealAmount').value
+  const aptCode = document.querySelector('#aptCode').value
+
+  const saveData = {}
+  saveData.no = no
+  saveData.houseName = houseName
+  saveData.sigunguCode = sigunguCode
+  saveData.floor = floor
+  saveData.area = area
+  saveData.dealYear = dealYear
+  saveData.dealMonth = dealMonth
+  saveData.dealDay = dealDay
+  saveData.dealAmount = dealAmount
+  saveData.aptCode = aptCode
+
+  try {
+    axios
+      .post('http://localhost:8080/house', JSON.stringify(saveData), {
+        headers: {
+          "Authorization" : localStorage.getItem("Authorization"),
+          'Content-Type': `application/json`
+        }
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(saveData)
+        }
+      })
+  } catch (error) {
+    console.error(error)
+  }
+  postAptModal.close()
 }
 
 const goHome = () => {
@@ -260,11 +424,11 @@ const login = () => {
     axios
       .post('http://localhost:8080/users/login', JSON.stringify(saveData), {
         headers: {
+          
           'Content-Type': `application/json`
         }
       })
       .then((res) => {
-        console.log(res.headers.role)
         if (res.status === 200) {
           loggedIn.value = true
           localStorage.setItem('Authorization', res.headers.get('Authorization'))
@@ -289,17 +453,17 @@ const login = () => {
 
 const logout = () => {
   const loginId = document.querySelector('#loginid').value
-
   loggedIn.value = false
+  const data = null
   try {
     axios
-      .post('http://localhost:8080/users/logout/' + loginId, {
+      .post('http://localhost:8080/users/logout' , data,{
         headers: {
+          "Authorization" : localStorage.getItem("Authorization"),
           'Content-Type': `application/json`
         }
       })
       .then((res) => {
-        //// console.log(res)
         if (res.status === 200) {
           loggedIn.value = false
           localStorage.removeItem('Authorization')
